37684d2622a0ce20c8527397a158275a3ef56b7c
Raphael C <polyster20@gmail.com>
2024-01-02 15:39:50 +0100

    WIP

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styled from 'styled-components';
+import SButton from './_components/SButton';
 
 function View() {
   const styles = {
@@ -17,9 +18,32 @@ function View() {
   )
 }
 
+function MenuGender() {
+  return <div>MenuGender</div>
+}
 
+function MenuHair() {
+  return <div>MenuHair</div>
+}
+
+function MenuEyes() {
+  return <div>MenuEyes</div>
+}
+
+function MenuEyesbrows() {
+  return <div>MenuEyesbrows</div>
+}
+
+function MenuBeard() {
+  return <div>MenuBeard</div>
+}
+
+function MenuShape() {
+  return <div>MenuShape</div>
+}
 
 function Controller() {
+  const [selected, setSelected] = React.useState('gender');
   const ControllerContainer = styled.div`
     width: 50vw;
     height: 80vh;
@@ -27,62 +51,58 @@ function Controller() {
     Position: absolute;
     top: calc(50vh - 40vh);
     left: 45%;
-    border: 1px solid red;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
   `
 
-  const SButton = ({selected, children, tab}: {selected?: boolean, children: React.ReactNode, tab?: boolean}) => {
-    const color = selected ? '#FFFEFF' : '#707476';
-    const StyledButton = styled.button`
-      width: 250px;
-      height: ${tab ? '70px': '50px'};
-      background-color: ${selected ? '#080C10' : 'transparent'};
-      color: #707476;
-      border: 2px solid ${color};
-      border-right: ${tab ? `8px solid ${color}` : 'auto'};
-      transition: 0.2s;
-      outline: none;
-      cursor: pointer;
-      display: flex;
-      align-items: center;
-      justify-content: ${tab ? 'flex-start' : 'center'};
-      padding-left: ${tab ? '20px' : '0'};
-      &:hover {
-        color: #FFFEFF;
-        border: 2px solid #FFFEFF;
-        border-right: ${tab ? `8px solid #FFFEFF` : 'auto'};
-        transition: 0.2s;
-      }
-      &:active {
-        color: #B5B9BA;
-        border: 2px solid #B5B9BA;
-        border-right: ${tab ? `8px solid #B5B9BA` : 'auto'};
-        transition: 0.2s;
-      }
-    `;
+  const tab = [
+    { key: 'gender', value: 'Genre', Component: MenuGender },
+    { key: 'hair', value: 'Cheveux', Component: MenuHair },
+    { key: 'eyes', value: 'Yeux', Component: MenuEyes },
+    { key: 'eyesbrows', value: 'Sourcils', Component: MenuEyesbrows },
+    { key: 'beard', value: 'Barbe', Component: MenuBeard},
+    { key: 'shape', value: 'Corpulence', Component: MenuShape}
+  ]
 
-    const handleClick = () => {
-      const audio = new Audio('/assets/sounds/click.ogg');
-      audio.play();
-
-    }
-
-    return (
-      <StyledButton onClick={handleClick}>
-        <p style={{
-          fontSize: '1.6em',
-          textTransform: 'uppercase',
-        }}>
-          {children}
-        </p>
-      </StyledButton>
-    )
-  }
 
   return (
     <ControllerContainer>
-      Controller
-      <SButton tab>Genre</SButton>
-      <SButton>Terminer</SButton>
+      <div
+        style={{
+          width: '350px',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '20px',
+          alignItems: 'center',
+        }}
+      >
+        <p style={{fontSize: '1.8em', width: '280px', userSelect: 'none'}}>
+          Customise ton apparence
+        </p>
+
+        <div
+          style={{display: 'flex', flexDirection: 'column', gap: '5px'}}
+        >
+          {tab.map((item, index) => <SButton 
+            key={index}
+            tab
+            selected={selected === item.key ? true : false}
+            onClick={() => setSelected(item.key)}
+          >
+            {item.value}
+          </SButton>)}
+        </div>
+
+        <SButton>Terminer</SButton>
+
+      </div>
+
+      <div style={{width: '100%', height: '100%', padding: '20px', border: '1px solid green'}}>
+        {tab.filter(item => item.key === selected)[0].Component()}
+      </div>
+
     </ControllerContainer>
   )
 }
