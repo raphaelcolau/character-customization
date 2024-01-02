6d0e983f4f6419bd51d9717c3ad25a119f6e115d
Raphael C <polyster20@gmail.com>
2024-01-02 22:21:26 +0100

    Update Title.tsx

--- src/app/_components/Title.tsx
+++ src/app/_components/Title.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Title = styled.p`
+    white-space: nowrap;
     color: #707476;
     font-size: 1.8em;
     user-select: none;
-    width: fit-content;
 `
