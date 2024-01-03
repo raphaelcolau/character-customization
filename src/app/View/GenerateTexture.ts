24177bfc6235982456da3fb3fe89e0b3cb043c98
Raphael C <polyster20@gmail.com>
2024-01-03 18:28:57 +0100

    Update GenerateTexture.ts

--- src/app/View/GenerateTexture.ts
+++ src/app/View/GenerateTexture.ts
@@ -3,18 +3,51 @@ import { Character } from '../../type/type';
 import { hairs, eyes, eyesBrown, beards, skinShape } from '../_assets/assets';
 
 
-async function loadImageAndDraw(toDraw: string, ctx: CanvasRenderingContext2D, color?: string) {
-
+async function loadImageAndDraw(toDraw: string, ctx: CanvasRenderingContext2D, color?: string, mixType?: GlobalCompositeOperation) {
     return new Promise((resolve, reject) => {
-      const img = new Image();
-      img.src = toDraw ? toDraw : '';
-      img.onload = () => {
-        ctx.drawImage(img, 0, 0, 64, 64);
-        resolve(ctx);
-      };
-      img.onerror = reject;
+        const img = new Image();
+        img.src = toDraw ? toDraw : '';
+        img.onload = () => {
+            if (color && mixType) {
+                const tempCanvas = document.createElement('canvas');
+                tempCanvas.width = 64;
+                tempCanvas.height = 64;
+                const tempCtx = tempCanvas.getContext('2d');
+                if (tempCtx) {
+                    // tempCtx.fillStyle = color;
+                    // tempCtx.fillRect(0, 0, 64, 64);
+
+                    //Loop every pixels of img
+                    for (let i = 0; i < 64; i++) {
+                        for (let j = 0; j < 64; j++) {
+                            //Get the pixel color
+                            const pixel = tempCtx.getImageData(i, j, 1, 1).data;
+                            
+                            //If pixel is not transparent
+                            console.log(pixel);
+                            if (pixel[3] !== 0) {
+                                //Apply the color
+                                tempCtx.fillStyle = color;
+                                tempCtx.fillRect(i, j, 1, 1);
+                            }
+                        }
+                    }
+
+                    tempCtx.globalCompositeOperation = mixType;
+                    tempCtx.drawImage(img, 0, 0, 64, 64);
+
+
+                    ctx.drawImage(tempCanvas, 0, 0, 64, 64);
+                }
+            } else {
+                ctx.drawImage(img, 0, 0, 64, 64);
+            }
+            console.log(ctx.canvas.toDataURL());
+            resolve(ctx);
+        };
+        img.onerror = reject;
     });
-  }
+}
 
 export async function generateTexture(custom: Character): Promise<THREE.CanvasTexture> {
     return new Promise((resolve, reject) => {
@@ -31,8 +64,8 @@ export async function generateTexture(custom: Character): Promise<THREE.CanvasTe
         const ctx = canvas.getContext('2d');
         if (ctx) {
             if (skinUrl && hairUrl && eyesUrl && eyesBrownUrl && beardsUrl) {
-                loadImageAndDraw(skinUrl, ctx).then(() => {
-                    loadImageAndDraw(hairUrl, ctx).then(() => {
+                loadImageAndDraw(skinUrl, ctx, custom.skin.color, 'color-burn').then(() => {
+                    loadImageAndDraw(hairUrl, ctx, custom.skin.color, 'color-dodge').then(() => {
                         loadImageAndDraw(eyesUrl, ctx).then(() => {
                             loadImageAndDraw(eyesBrownUrl, ctx).then(() => {
                                 loadImageAndDraw(beardsUrl, ctx).then(() => {
